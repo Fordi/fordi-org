ac6fe0fdc3e4eb613b73cced937c183fef0fc662
Bryan Elliott <fordiman@gmail.com>
2020-07-07 22:17:54 -0400

    Changes for building in Windows.

--- dist.sh
+++ dist.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 rm -rf dist
-mkdir 'vendor'
-wget 'https://unpkg.com/@fordi-org/buildless@1.0.2/dist/buildless.modern.js' -qO 'vendor/buildless.js'
 npx microbundle \
   -i src/index.js \
   -o dist/index.js \
-  --alias 'https://unpkg.com/@fordi-org/buildless@1.0.2/dist/buildless.modern.js'='../vendor/buildless.js' \
-  --raw --compress \
+  --compress \
   -f modern
-rm -rf 'vendor';
 mv dist/index.modern.js dist/index.js
 gzip -9k dist/index.js
 gzip -9k dist/index.modern.js.map
+FMT="--format %s" && stat $FMT .git/config > /dev/null 2>&1 || FMT="-f %z"
 while read file; do
   mkdir -p "dist/$(dirname "$file")"
   cp "src/$file" "dist/$file"
   gzip -9k "dist/$file"
-  if [[ "$(stat -f %z "dist/$file")" -lt "$(stat -f %z "dist/$file.gz")" ]]; then
+  if [[ "$(stat $FMT "dist/$file")" -lt "$(stat $FMT "dist/$file.gz")" ]]; then
     rm "dist/$file.gz"
   fi
 done < <(find src -type f -not -iname *.js | cut -d / -f 2-)
